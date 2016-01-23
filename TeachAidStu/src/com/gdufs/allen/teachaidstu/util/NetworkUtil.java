15c451023744448a3221c6aba482434c551ad3cd
LinQinan <742890434@qq.com>
2016-01-23 16:04:27 +0800

    t

--- TeachAidStu/src/com/gdufs/allen/teachaidstu/util/NetworkUtil.java
+++ TeachAidStu/src/com/gdufs/allen/teachaidstu/util/NetworkUtil.java
@@ -4,22 +4,30 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo.State;
 
+/**
+ * @author Allen Lin
+ * @date 2016-1-21
+ * @desc
+ */
 public class NetworkUtil {
-	
+
 	public final static int NONE = 0;
 	public final static int WIFI = 1;
 	public final static int MOBILE = 2;
-	
+
 	public static int getNetworkState(Context context) {
-		
-		ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-		
-		State state = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
-		if(state == State.CONNECTED || state == State.CONNECTING) {
+
+		ConnectivityManager connManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		State state = connManager.getNetworkInfo(
+				ConnectivityManager.TYPE_MOBILE).getState();
+		if (state == State.CONNECTED || state == State.CONNECTING) {
 			return MOBILE;
 		}
-		state = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
-		if(state == State.CONNECTED || state == State.CONNECTING) {
+		state = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+				.getState();
+		if (state == State.CONNECTED || state == State.CONNECTING) {
 			return WIFI;
 		}
 		return NONE;
