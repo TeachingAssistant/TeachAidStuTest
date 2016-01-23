65e59119142e0bfada5d2308634e11314cbcf767
LinQinan <742890434@qq.com>
2016-01-23 16:24:26 +0800

    s

--- TeachAidStu/src/com/gdufs/allen/teachaidstu/util/ToastUtil.java
+++ TeachAidStu/src/com/gdufs/allen/teachaidstu/util/ToastUtil.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToastUtil {
-	
+
 	public static void showLongToast(Context context, String message) {
-		
+
 		Toast.makeText(context, message, Toast.LENGTH_LONG).show();
 	}
-	
+
 	public static void showShortToast(Context context, String message) {
 		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 	}
