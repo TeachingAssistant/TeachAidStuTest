85b85b7746f32363d29b9c0659893a7134827411
LinQinan <742890434@qq.com>
2016-01-27 23:51:29 +0800

    注释

--- TeachAidStu/src/com/gdufs/allen/teachaidtch/util/ToastUtil.java
+++ TeachAidStu/src/com/gdufs/allen/teachaidtch/util/ToastUtil.java
@@ -3,6 +3,11 @@ package com.gdufs.allen.teachaidtch.util;
 import android.content.Context;
 import android.widget.Toast;
 
+/**
+ * @author Allen Lin
+ * @date 2016-1-21
+ * @desc Toast工具
+ */
 public class ToastUtil {
 
 	public static void showLongToast(Context context, String message) {
